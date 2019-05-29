4d45c1b7910e76a6499ef7265df971fd00fee8c3
Ömer Ziemke <omarziemke@gmail.com>
2019-05-29 14:20:35 +0300

    initial commit

---
+++ resources/views/index/index.blade.php
@@ -0,0 +1,7 @@
+@extends('index.home')
+@section('content')
+    <div class="panel panel-default">
+        <div class="panel-heading">Kitap Listesi</div>
+        <div class="panel-body">icerik</div>
+    </div>
+    @endsection
